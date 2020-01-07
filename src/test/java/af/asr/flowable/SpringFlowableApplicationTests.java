db99ec01da6942ffb25f0fcb9ea4192b04675a16
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-07 09:13:32 +0430

    setup

---
+++ src/test/java/af/asr/flowable/SpringFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlowableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
